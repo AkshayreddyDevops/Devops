c1188a2f80d71b055a8477b1dedc4948682f1fc6
reddy.akshay.gaddam <reddy.akshay.gaddam@gmail.com>
2024-07-06 14:22:06 +0530

    cart

--- cart.sh
+++ cart.sh
@@ -1,6 +1,6 @@
-source commoh.sh
+source common.sh
 
-cp cart.service /etc/systemd/system/cart.sevice
+cp cart.service /etc/systemd/system/cart.service
 nodeJS
 rm -rf /app
 mkdir /app
@@ -8,6 +8,7 @@ curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
 unzip /tmp/cart.zip
 npm install
+useradd roboshop
 systemctl daemon-reload
 systemctl enable cart
 systemctl start cart
