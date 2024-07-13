c2acddb47f93d5e52f3c2258dac155638642e681
reddy.akshay.gaddam <reddy.akshay.gaddam@gmail.com>
2024-07-14 00:09:23 +0530

    users

--- deploy_component.sh
+++ deploy_component.sh
@@ -1,2 +1,2 @@
 git pull
-ansible-playbook -i $1, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role=$2 -e redis_ip=172.31.87.171 -e mongo_id=172.31.82.151 deploy_components.yml 
+ansible-playbook -i $1, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role=$2 -e redis_ip=172.31.87.171 -e mongo_ip=172.31.82.151 deploy_components.yml 
