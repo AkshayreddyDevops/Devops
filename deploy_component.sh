3323c85fea347c522166863cebae9f1c841e42ec
reddy.akshay.gaddam <reddy.akshay.gaddam@gmail.com>
2024-07-14 01:45:40 +0530

    cart

--- deploy_component.sh
+++ deploy_component.sh
@@ -1,2 +1,2 @@
 git pull
-ansible-playbook -i $1, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role=$2 -e redis_ip=172.31.87.171 -e mongo_ip=172.31.82.151 -e catalogue_ip=172.31.81.21 -e cart_ip=172.31.81.110 mysql_ip=172.31.92.157 deploy_components.yml 
+ansible-playbook -i $1, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role=$2 -e redis_ip=172.31.87.171 -e mongo_ip=172.31.82.151 -e catalogue_ip=172.31.81.21 -e cart_ip=172.31.81.110 -e mysql_ip=172.31.92.157 deploy_components.yml 
