ad5b2274bc952aeef15e1768eedc1b96f647eb0c
reddy.akshay.gaddam <reddy.akshay.gaddam@gmail.com>
2024-07-18 14:21:21 +0530

    tf instance creation

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,14 +1,3 @@
-variable "ami" {
-  default = "ami-041e2ea9402c46c32"
-}
-
-variable "instance_type" {
-  default = "t3.micro"
-}
-
-variable "sg" {
-  default = ["sg-0690f9908ac885f47"]
-}
 resource "aws_instance" "frontend" {
   ami           =  var.ami
   instance_type = var.instance_type
