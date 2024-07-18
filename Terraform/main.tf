31158336a82bdffcc0279c897534d4315cd39372
reddy.akshay.gaddam <reddy.akshay.gaddam@gmail.com>
2024-07-18 17:13:59 +0530

    zoneid

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,8 +2,28 @@ resource "aws_instance" "frontend" {
   ami           =  data.aws_ami.ami.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = data.aws_security_groups.sg.ids
-
   tags = {
     Name = "frontend"
+  }
+}
+
+
+resource "aws_instance" "mongo" {
+  ami           =  data.aws_ami.ami.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = data.aws_security_groups.sg.ids
+
+  tags = {
+    Name = "mongo"
+  }
+}
+
+resource "aws_instance" "catalogue" {
+  ami           =  data.aws_ami.ami.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = data.aws_security_groups.sg.ids
+
+  tags = {
+    Name = "catalogue"
   }
 }
