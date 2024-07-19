db453b75ce9e49a25d675fff9f8612aa0b1b19ed
reddy.akshay.gaddam <reddy.akshay.gaddam@gmail.com>
2024-07-19 17:36:37 +0530

    test opt

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,11 +1,23 @@
-resource "aws_instance" "instance" {
-  for_each = var.components
-  ami           =  data.aws_ami.ami.image_id
-  instance_type = each.key["instance_type"]
-  vpc_security_group_ids = data.aws_security_groups.sg.ids
+# resource "aws_instance" "instance" {
+#   for_each = var.components
+#   ami           =  data.aws_ami.ami.image_id
+#   instance_type = each.key["instance_type"]
+#   vpc_security_group_ids = data.aws_security_groups.sg.ids
+#   tags = {
+#     Name = "${each.key}.dev"
+#   }
+# }
+
+
+resource "aws_instance_tst" "display"{
+  ami = "RHEL-9-DevOps-Practice"
+  instance_type = "t3.micro"
+  vpc_security_group_ids  = "allow-all"
   tags = {
-    Name = "${each.key}.dev"
+    name = "frontend"
   }
 }
 
-
+output "name" {
+  value = aws_instance_tst.display
+}
