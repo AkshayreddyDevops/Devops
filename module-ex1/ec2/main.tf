a9cfe3926545775bf05186e650e04910dcc6ac64
reddy.akshay.gaddam <reddy.akshay.gaddam@gmail.com>
2024-07-20 12:48:58 +0530

    modele test1

---
+++ module-ex1/ec2/main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "test"{
+  ami = "ami-041e2ea9402c46c32"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0690f9908ac885f47"]
+  tags = {
+    name = "test"
+  }
+}
